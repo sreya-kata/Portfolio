c92a6d6044ee8a896ce67372db5b5d88df096d3b
sreya-kata <sreya.kata@gmail.com>
2021-04-07 23:11:19 -0400

    got rid of magnific popup stuff

--- js/main.js
+++ js/main.js
@@ -1,57 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  let $btns = $(".project-area .container .button-group button");
 
-  let $btns = $('.project-area .container .button-group button');
+  $btns.click(function (e) {
+    $(".project-area .container .button-group button").removeClass("active");
+    e.target.classList.add("active");
 
-  $btns.click(function(e){
-    $('.project-area .container .button-group button').removeClass('active');
-    e.target.classList.add('active');
-
-    let selector = $(e.target).attr('data-filter');
-    $('.project-area .grid').isotope({
-      filter: selector
+    let selector = $(e.target).attr("data-filter");
+    $(".project-area .grid").isotope({
+      filter: selector,
     });
 
     return false;
-  })
-
-  $('project-area button-group #btn1').trigger('click');
-
-  $('.project-area .grid .test-popup-link').magnificPopup({
-    type: 'image',
-    gallery:{enabled:true}
   });
 
-//owl carousel
-$('.site-main .fun-facts .owl-carousel').owlCarousel({
-  loop: true,
-  autoplay: true,
-  dots: true,
-  responsive: {
-    0: {
-      items: 1
+  $("project-area button-group #btn1").trigger("click");
+
+  //owl carousel
+  $(".site-main .fun-facts .owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      544: {
+        items: 2,
+      },
     },
-    544: {
-      items: 2
+  });
+
+  //sticky navIAGTion
+  let nav_offset_top = $(".heading").height() + 50;
+
+  function navbarFixed() {
+    if ($(".heading").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= nav_offset_top) {
+          $(".heading .main-menu").addClass("navbar_fixed");
+        } else {
+          $(".heading .main-menu").removeClass("navbar_fixed");
+        }
+      });
     }
   }
-})
 
-    //sticky navIAGTion
-    let nav_offset_top = $('.heading').height() + 50;
-
-    function navbarFixed() {
-      if($('.heading').length){
-        $(window).scroll(function(){
-          let scroll = $(window).scrollTop();
-          if(scroll >= nav_offset_top){
-            $('.heading .main-menu').addClass('navbar_fixed');
-          } else {
-            $('.heading .main-menu').removeClass('navbar_fixed');
-          }
-        })
-      }
-    }
-
-    navbarFixed();
-
+  navbarFixed();
 });
